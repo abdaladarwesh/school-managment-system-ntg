dd7f185c10714b55e8af27cc9dd25b0b645e0f75
RodinaMohamed2009 <rhsn6786@gmail.com>
2026-06-25 20:48:00 +0300

    updated

---
+++ backend/src/main/java/com/ntg/sms/Security/CustomUserDetails.java
@@ -0,0 +1,53 @@
+package com.ntg.sms.Security;
+
+import lombok.RequiredArgsConstructor;
+import com.ntg.sms.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+@RequiredArgsConstructor
+
+public class CustomUserDetails  implements UserDetails{
+    private final  User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority("Role_"+user.getRole().getRoleName())
+        );
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !Boolean.TRUE.equals(user.getIsdeleted());
+    }
+
+}
